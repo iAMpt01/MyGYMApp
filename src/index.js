977cf3e33fd5247dc8cb33842bb0d42d34826996
iAMpt <trifpatricia@yahoo.com>
2023-03-25 19:59:27 +0200

    ADD

--- src/index.js
+++ src/index.js
@@ -3,15 +3,41 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+/// 1. import react-redux and redux
+import {createStore, combineReducers} from 'redux'
+import {Provider} from 'react-redux'
+import { composeWithDevTools } from 'redux-devtools-extension';
+/// 2. create user reducer function
+// {type:'logged', payload:{name:Rayan, role:'seller'}}
+const authReducer = (state={}, action) => {
+    switch(action.type){
+      case "Logged_in-user":
+        return{...state, ...action.payload};
+      case "Logout":
+        return action.payload;
+      default:
+        return state;
+    }
+};
+
+/// 3. combine multiple reducers
+const rootreducer = combineReducers({
+    user: authReducer,
+});
+
+/// 4. create redux store
+const store = createStore(rootreducer, composeWithDevTools());
+
+/// 5. provice redux store to the entire app
 
 ReactDOM.render(
 <React.StrictMode>
+  <Provider store={store}>
   <App />
+  </Provider>
 </React.StrictMode>,
 document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
